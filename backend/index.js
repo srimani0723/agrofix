3376253d74626141cb2a4bf8ecc6386cfe8cae3a
Srimanikanta <122541961+srimani0723@users.noreply.github.com>
2025-04-20 18:33:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from frontend
+    origin: ["http://localhost:5173","https://agrofix-5hlb.onrender.com"] // Allow requests from frontend
   })
 );
 
